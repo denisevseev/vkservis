49ad3104243d4dd0cea5b6090aee908458fdb432
denis <tv_bb@mail.ru>
2022-12-01 03:24:24 +0300

    хром обновился этого я не ждал

--- src/server/autorize.js
+++ src/server/autorize.js
@@ -17,13 +17,14 @@ class autorize_class {
       const chrome = require("selenium-webdriver/chrome");
       const chromedriver = require("chromedriver");
       this.driver = new webdriver.Builder()
-        .setChromeOptions(new chrome.Options().headless())
+          //.setChromeOptions(new chrome.Options().headless())
         .forBrowser("chrome")
         .build();
-      await this.driver
-        .get(
-          "https://oauth.vk.com/authorize?client_id=51399251&display=page&scope=wall,photos,groups,friends,video,market,email,offline&response_type=token&v=5.131"
-        )
+      await this.driver.get('https://google.com')
+      // await this.driver
+      //   .get(
+      //     "https://oauth.vk.com/authorize?client_id=51399251&display=page&scope=wall,photos,groups,friends,video,market,email,offline&response_type=token&v=5.131"
+      //   )
         .then((success, error) => {
           if (error) {
             console.log(error);
