a8bcd13a32f2ee2085d264862db0fbe6c4976361
denis <tv_bb@mail.ru>
2023-05-28 00:58:58 +0300

    added headers in requests.js

--- src/server/requests.js
+++ src/server/requests.js
@@ -16,6 +16,11 @@ const instance = axios.create({
   },
 });
 
+// const randomValue = Math.random() < 0.5 ? "axios" : "instance";
+// console.log(randomValue);
+
+
+
 // const httpsAgent = new https.Agent({ keepAlive: true });
 const url = `https://api.vk.com/method/`;
 const posts_request = async (data) => {
@@ -25,7 +30,13 @@ const posts_request = async (data) => {
       `${url}wall.post?&owner_id=-${encodeURI(
         data.owner_id
       )}&message=${encodeURI(data.message)}&access_token=${data.token}&v=5.131`,
-      { timeout: 5000 }
+      { timeout: 5000,
+        headers:{
+          'Content-Type': 'application/json',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.3.3.721 Yowser/2.5 Safari/537.36'
+        }
+
+      }
     )
     .catch((err) => console.log(err, "BAN VK!!!!!!!!!"));
   return response.data;
@@ -33,7 +44,7 @@ const posts_request = async (data) => {
 
 const wallDelete = async (data) => {
   //запрос на удаление записи со стены
-  let response = await instance.get(
+  let response = await axios.get(
     `${url}wall.delete?owner_id=${data.owner_id}&post_id=${data.post_id}&access_token=${data.token}&v=5.131`
   );
   console.log(response.data);
@@ -42,7 +53,7 @@ const wallDelete = async (data) => {
 
 const wallDeleteComment = async (data) => {
   //запрос на удаление комментария со стены
-  let response = await instance.get(
+  let response = await axios.get(
     `${url}wall.deleteComment?owner_id=-${data.owner_id}&comment_id=${data.comment_id}&access_token=${data.token}&v=5.131`
   );
   return response.data;
@@ -50,7 +61,7 @@ const wallDeleteComment = async (data) => {
 
 const wallGetComments = async (data) => {
   //запрос получение ид комментво к записи на стене
-  let response = await instance.get(
+  let response = await axios.get(
     `${url}wall.getComments?owner_id=-${data.owner_id}&post_id=${data.post_id}&access_token=${data.token}&v=5.131`
   );
   return response.data;
@@ -58,7 +69,7 @@ const wallGetComments = async (data) => {
 
 const createComment = async (data) => {
   let message = encodeURI(data.message);
-  let response = await instance.get(
+  let response = await axios.get(
     // оставление комментария на стене группы с известным айди группы и айди поста
     `${url}wall.createComment?&owner_id=${data.owner_id}&post_id=${data.post_id}&message=${message}&access_token=${data.token}&v=5.131`
   );
@@ -66,7 +77,7 @@ const createComment = async (data) => {
 };
 
 const joinGroups = async (owner_id, token) => {
-  let response = await instance.get(
+  let response = await axios.get(
     // вступление в группу
     `${url}groups.join?&group_id=${owner_id}&access_token=${token}&v=5.131`
   );
@@ -75,7 +86,7 @@ const joinGroups = async (owner_id, token) => {
 
 const search = async (data) => {
   console.log(data.inputValue[data.arr_str_for_search], "req!!!");
-  let search = await instance.get(
+  let search = await axios.get(
     `${url}groups.search?${
       data.city ? `city_id=${data.city}` : ""
     }&count=1000&q=${encodeURI(
@@ -86,7 +97,7 @@ const search = async (data) => {
 };
 
 const Filter_group = async (arr, token) => {
-  let filter = await instance.get(
+  let filter = await axios.get(
     // фильтр групп на возможность постинга на стену и фильтр колич-ва подписчиков группы
     `${url}groups.getById?&group_ids=${arr}&fields=can_post,members_count,city,country,description&access_token=${token}&v=5.131`
   );
@@ -95,7 +106,7 @@ const Filter_group = async (arr, token) => {
 
 const canComments = async (owner_id, token) => {
   // запрос на возможность оставления комментария на стене
-  let response = await instance.get(
+  let response = await axios.get(
     `${url}wall.get?&owner_id=-${owner_id}&extended=1&fields=can_post,is_closed&access_token=${token}&v=5.131`, { timeout: 5000 }
   )
 .catch((err) => console.log(err, "err wall.get!!!"));
