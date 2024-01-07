c9e1eff8280d386351171187167a38635d14e307
denis <tv_bb@mail.ru>
2024-01-07 21:38:46 +0300

    new interface

---
+++ src/client/options/Options.js
@@ -0,0 +1,30 @@
+const Country = [
+  { value: "any", label: "Любая страна" },
+  { value: "ru", label: "Россия" },
+  { value: "be", label: "Белоруссия" },
+  { value: "uk", label: "Украина" },
+];
+const City = [
+  { value: "any", label: "Любой город" },
+  { value: "1", label: "Москва" },
+  { value: "2", label: "Санкт-Петербург" },
+  { value: "95", label: "Нижний новгород" },
+  { value: "99", label: "Новосибирск" },
+  { value: "49", label: "Екатиринбург" },
+  { value: "60", label: "Казань" },
+  { value: "141", label: "Тверь" },
+  { value: "133", label: "Сочи" },
+  { value: "72", label: "Краснодар" },
+];
+const Groups = [
+  { value: "", label: "Открытые и закрытые сообщества" },
+  { value: "0", label: "Только открытые сообщества" },
+  { value: "1", label: "Только закрытые сообщества" },
+];
+const Groups2 = [
+  { value: "", label: "Все типы сообществ" },
+  { value: "group", label: "Только группы" },
+  { value: "page", label: "Только публичные страницы" },
+  { value: "event", label: "Только мероприятия (встречи)" },
+];
+export { Country, City, Groups, Groups2 };
